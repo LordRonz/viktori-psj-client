2f9c3a4fd23b43d25d244123a36797616fa56d24
LordRonz <aarontanhar2000@gmail.com>
2021-11-20 11:06:36 +0700

    feat: Sends serial data to server via TCP socket

--- client.c
+++ client.c
@@ -6,9 +6,10 @@ void run(int argc, char **argv) {
     char *echo_string;                /* String to send to echo server */
     char echo_buffer[RCVBUFSIZE];     /* Buffer for echo string */
     unsigned int echo_string_len;      /* Length of string to echo */
-    int bytes_rcvd, total_bytes_rcvd;   /* Bytes read in single recv() 
+    int bytes_rcvd, total_bytes_rcvd;   /* Bytes read in single recv()
                                         and total bytes read */
     struct addrinfo hints, *res;
+    int serial_fd;
 
     echo_string = argv[2];         /* Second arg: string to echo */
 
@@ -32,34 +33,44 @@ void run(int argc, char **argv) {
         die_with_error("socket() failed");
     }
 
+    echo_string = malloc(sizeof (char) * SENDBUFSIZE);
+    serial_fd = open_serial();
+
+
     /* Establish the connection to the echo server */
     if (connect(sock, res->ai_addr, res->ai_addrlen) < 0) {
         die_with_error("connect() failed");
     }
 
-    echo_string_len = strlen(echo_string);     /* Determine input length */
+    for(;;) {
+        echo_string_len = read_serial(serial_fd, echo_string, SENDBUFSIZE);
 
-    /* Send the string to the server */
-    if (send(sock, echo_string, echo_string_len, 0) != echo_string_len) {
-        die_with_error("send() sent a different number of bytes than expected");
+        echo_string_len = strlen(echo_string);     /* Determine input length */
+
+        /* Send the string to the server */
+        if (send(sock, echo_string, echo_string_len, 0) != echo_string_len) {
+            die_with_error("send() sent a different number of bytes than expected");
+        }
+
+        /* Receive the same string back from the server */
+        total_bytes_rcvd = 0;
+        printf("Received: ");                /* Setup to print the echoed string */
+
+        /* Receive up to the buffer size (minus 1 to leave space for
+            a null terminator) bytes from the sender */
+        bytes_rcvd = recv(sock, echo_buffer, RCVBUFSIZE - 1, 0);
+        if (bytes_rcvd < 1) {
+            die_with_error("recv() failed or connection closed prematurely");
+        }
+        total_bytes_rcvd += bytes_rcvd;   /* Keep tally of total bytes */
+        echo_buffer[bytes_rcvd] = '\0';  /* Terminate the string! */
+        printf("%s", echo_buffer);      /* Print the echo buffer */
+
+        puts("");    /* Print a final linefeed */
     }
 
-    /* Receive the same string back from the server */
-    total_bytes_rcvd = 0;
-    printf("Received: ");                /* Setup to print the echoed string */
-
-    /* Receive up to the buffer size (minus 1 to leave space for
-        a null terminator) bytes from the sender */
-    bytes_rcvd = recv(sock, echo_buffer, RCVBUFSIZE - 1, 0);
-    if (bytes_rcvd < 1) {
-        die_with_error("recv() failed or connection closed prematurely");
-    }
-    total_bytes_rcvd += bytes_rcvd;   /* Keep tally of total bytes */
-    echo_buffer[bytes_rcvd] = '\0';  /* Terminate the string! */
-    printf("%s", echo_buffer);      /* Print the echo buffer */
-
-    puts("");    /* Print a final linefeed */
-
+    free(echo_string);
+    close(serial_fd);
     close(sock);
 }
 
